7b43917936e1d11a9742dfcd6882481517ca55e4
jinhojang6 <jinho5051@gmail.com>
2024-07-03 02:49:00 +0900

    chore: update og image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -9,9 +9,6 @@ export const config = {
 // Doc: https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#using-a-local-image
 // Font: https://vercel.com/docs/functions/edge-functions/og-image-generation/og-image-examples#using-a-custom-font
 export default async function handler(request: NextRequest) {
-  const helvetica = await fetch(
-    new URL('../../../public/fonts/Helvetica.ttf', import.meta.url),
-  ).then((res) => res.arrayBuffer())
   const { href } = request.nextUrl
 
   const searchParams = new URL(href).searchParams
@@ -29,15 +26,16 @@ export default async function handler(request: NextRequest) {
           backgroundColor: '#fff',
           color: '#1400ff',
           position: 'relative',
-          fontFamily: 'Helvetica',
-          padding: '40px',
+          fontFamily: 'sans-serif',
+          padding: '39px 45px',
         }}
       >
         <div
           style={{
             display: 'flex',
             flexDirection: 'column',
-            gap: '4px',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           <div
@@ -45,37 +43,53 @@ export default async function handler(request: NextRequest) {
               display: 'flex',
               alignItems: 'flex-start',
               fontSize: '54px',
+              lineHeight: '73px',
+              fontWeight: '400',
             }}
           >
-            Acid.info
+            {pagePath === '/' ? null : pagePath === '/media' ? (
+              <div>Media</div>
+            ) : pagePath === '/articles' ? (
+              <div>Articles</div>
+            ) : pagePath === '/about' ? (
+              <div>About</div>
+            ) : pagePath === '/shop' ? (
+              <div>Shop</div>
+            ) : null}
           </div>
 
-          <div style={{ fontSize: '42px', marginTop: '20px' }}>
-            The future is a second enlightenment of the digital world.
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontSize: '52px',
+              fontWeight: '400',
+              textAlign: 'center',
+              height: '100%',
+              width: '1073px',
+              margin: '0 auto',
+            }}
+          >
+            <div>Acid is designing the future.</div>
+            <div>
+              The future is a second enlightenment of the digital world.
+            </div>
+            {/* <img
+              src="/og/mark.svg"
+              width={'100%'}
+              height={'100%'}
+              alt="og-asset"
+              style={{ objectFit: 'cover' }}
+            /> */}
           </div>
-
-          {pagePath === '/' ? null : pagePath === '/media' ? (
-            <div style={{ fontSize: '50px', marginTop: '80px' }}>Media</div>
-          ) : pagePath === '/articles' ? (
-            <div style={{ fontSize: '50px', marginTop: '80px' }}>Articles</div>
-          ) : pagePath === '/about' ? (
-            <div style={{ fontSize: '50px', marginTop: '80px' }}>About</div>
-          ) : pagePath === '/shop' ? (
-            <div style={{ fontSize: '50px', marginTop: '80px' }}>Shop</div>
-          ) : null}
         </div>
       </div>
     ),
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: 'Helvetica',
-          data: helvetica,
-          style: 'normal',
-        },
-      ],
     },
   )
 }

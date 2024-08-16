f7f04df5bea667a6d4b8aef60fa27a60317add64
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2024-08-16 13:50:29 +0200

    feat: Add Webcave to the project (#3)
    * feature: add webcave to the project
    
    * chore: update webcave package
    
    * feat: update WebCave to newer version
    
    * hotfix: temporarily hard code public .env value

--- src/containers/Home/HomeContainer.tsx
+++ src/containers/Home/HomeContainer.tsx
@@ -1,7 +1,7 @@
 import { ClientOnly } from '@/components/ClientOnly'
 import ContentBox from '@/components/ContentBox/ContentBox'
-import { Minecraft } from '@/components/Minecraft'
 import { breakpoints } from '@/configs/ui.configs'
+import { WebCaveMultiplayer } from '@acid-info/webcave-react'
 import styled from '@emotion/styled'
 import React from 'react'
 import { useRecoilValue } from 'recoil'
@@ -21,7 +21,19 @@ export const HomeContainer: React.FC<HomePageProps> = ({
   return (
     <Container {...props}>
       <ClientOnly>
-        <Minecraft />
+        <WebCaveContainer>
+          <WebCaveMultiplayer
+            serverUrl={'https://webcave.infra.status.im/'} // Temporarily hard-coded
+            chunkSize={8}
+            texturePack={{
+              terrain: '/webcave/terrain.png',
+              player: '/webcave/player.png',
+              backgroundImage: '/webcave/background.png',
+              blockThumbsImage: '/webcave/blockthumbs.png',
+            }}
+            height={'100%'}
+          />
+        </WebCaveContainer>
       </ClientOnly>
       <ContentBox />
     </Container>
@@ -31,5 +43,30 @@ export const HomeContainer: React.FC<HomePageProps> = ({
 const Container = styled.div`
   @media (max-width: ${breakpoints.lg}px) {
     margin-inline: 10px;
+  }
+`
+
+const WebCaveContainer = styled.div`
+  @font-face {
+    font-family: 'Monocraft';
+    src: local('Monocraft'), url('/fonts/Monocraft.ttf') format('opentype');
+    font-weight: normal;
+  }
+
+  height: 450px;
+
+  * {
+    font-family: 'Monocraft' !important;
+  }
+
+  button {
+    background: none;
+    border: 0;
+    color: white;
+
+    &:hover {
+      border: 1px solid black;
+      border-radius: 8px;
+    }
   }
 `

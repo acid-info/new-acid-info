6bd528016d05d0b2b55040ee942d54bc14380e1a
jinhojang6 <jinho5051@gmail.com>
2025-07-16 00:17:16 +0900

    ffeat: bump react version to 19

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { SEO } from '@/components/SEO'
+import { HomeContainer } from '@/containers/Home'
 import { Button } from '@acid-info/lsd-react'
 
 export default function HomePage() {
   return (
     <>
       <SEO />
-      <div>hi</div>
       <Button>TEST</Button>
-      {/* <HomeContainer /> */}
+      <HomeContainer />
     </>
   )
 }
